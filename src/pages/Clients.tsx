f8317643edbf02e4f243a7d13f3d2fa0b92e1330
parshu462 <kumarprashant462021@gmail.com>
2025-03-17 22:43:46 +0530

    Updated Clients page , changed the review amount

--- src/pages/Clients.tsx
+++ src/pages/Clients.tsx
@@ -38,7 +38,7 @@ const Clients = () => {
     { 
       id: 'amazon', 
       name: 'Amazon Reviews', 
-      price: 40, 
+      price: 120, 
       image: 'https://images.unsplash.com/photo-1523474253046-8cd2748b5fd2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=300&q=80', 
       description: 'Increase your product visibility and sales with authentic Amazon reviews' 
     },
