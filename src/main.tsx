97e347fa70df0573d3fe0023a14e99cedc418195
parshu462 <kumarprashant462021@gmail.com>
2025-03-17 21:04:22 +0530

    Updated Clerk setup and added Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 
 import { createRoot } from 'react-dom/client'
 import { ClerkProvider } from "@clerk/clerk-react";
+import { Analytics } from "@vercel/analytics/react";
 import App from './App.tsx'
 import './index.css'
 
-const PUBLISHABLE_KEY = "pk_test_cHJlc2VudC1jcmFiLTI4LmNsZXJrLmFjY291bnRzLmRldiQ";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Clerk Publishable Key");
@@ -24,5 +27,6 @@ createRoot(document.getElementById("root")!).render(
     waitlistUrl="/"
   >
     <App />
+    <Analytics />
   </ClerkProvider>
 );
